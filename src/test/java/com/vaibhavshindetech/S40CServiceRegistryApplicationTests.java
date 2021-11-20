71cac833d7e49fc561d49d9731044283afb21afc
Vaibhav Shinde <vaibhavshinde1642@gmail.com>
2021-11-20 14:11:12 +0530

    initial commit

---
+++ src/test/java/com/vaibhavshindetech/S40CServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavshindetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S40CServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
